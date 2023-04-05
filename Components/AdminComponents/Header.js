151f1bc98f806eadeb0d3d9300b0b39e84988884
shohag roy <pkshohag240@gmail.com>
2023-04-05 18:25:10 +0600

    admin heder added

--- Components/AdminComponents/Header.js
+++ Components/AdminComponents/Header.js
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 import { FaMoon, FaBell } from "react-icons/fa";
 import { RxDashboard } from "react-icons/rx";
+import { AiOutlineLogout, AiFillSetting } from "react-icons/ai";
+import { GoThreeBars } from "react-icons/go";
 
 const Header = () => {
   const [openProfileModal, setOpenProfileModal] = useState(false);
 
   return (
     <nav className="shadow-lg">
-      <div className="max-w-[1440px] mx-auto p-3 flex items-center justify-between">
-        <div>{/* hambarger  */}</div>
-        <div className="flex items-center text-xl text-[#0E9F6E] ">
+      <div className="max-w-[1440px] mx-auto p-3 flex items-center justify-between text-xl text-[#0E9F6E]">
+        <div>
+          {/* hambarger  */}
+          <button className="block lg:hidden">
+            <GoThreeBars />
+          </button>
+        </div>
+        <div className="flex items-center  ">
           {/* button  */}
           <div className="mr-5">
             <button className="p-3 mx-2">
@@ -18,7 +25,7 @@ const Header = () => {
             <button className="p-3 relative">
               <FaBell />
               <div className="absolute -top-0 left-1 ">
-                <p className="flex justify-center text-sm items-center text-white bg-red rounded-full h-5 w-5">
+                <p className="flex justify-center text-sm items-center text-white bg-red-600 rounded-full h-5 w-5">
                   7
                 </p>
               </div>
@@ -37,22 +44,23 @@ const Header = () => {
               </button>
             </div>
 
+            {/* user pfoile modal  */}
             {openProfileModal && (
-              <div className="absolute bottom-100 rounded-lg shadow-lg right-0 text-gray-600 font-semibold  bg-white w-[250px]">
+              <div className="absolute bottom-100 rounded-lg shadow-lg right-0 text-gray-600 text-sm font-semibold  bg-white w-[250px]">
                 <div>
-                  <button className="w-full flex font-sm p-3 duration-300 hover:bg-gray-200 items-center">
+                  <button className="w-full flex items-center p-3 duration-300 hover:bg-gray-200 ">
                     <RxDashboard className="mr-2" />
                     <span className="">Dashbord</span>
                   </button>
 
                   <button className="w-full flex font-sm p-3 duration-300 hover:bg-gray-200 items-center">
-                    <RxDashboard className="mr-2" />
-                    <span className="">Dashbord</span>
+                    <AiFillSetting className="mr-2" />
+                    <span className="">Edit Profile</span>
                   </button>
 
                   <button className="w-full rounded-lg rounded-t-none flex font-sm p-3 duration-300 hover:bg-gray-200 items-center">
-                    <RxDashboard className="mr-2" />
-                    <span className="">Dashbord</span>
+                    <AiOutlineLogout className="mr-2" />
+                    <span className="">Log Out</span>
                   </button>
                 </div>
               </div>
