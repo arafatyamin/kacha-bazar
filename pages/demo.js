ab621adc4f31408ea2cb5af0a6c1bf26971ca599
Jahid0178 <zahidul.haque767@gmail.com>
2023-04-06 22:05:02 +0600

    complete category card

---
+++ pages/demo.js
@@ -0,0 +1,20 @@
+import CategoryCard from "@/Components/CustomerComponents/Cards/CategoryCard/CategoryCard";
+import { categoryItems } from "@/data/data";
+
+const demo = () => {
+  return (
+    <>
+      <section className="py-3">
+        <div className="container mx-auto px-3">
+          <div className="grid grid-cols-6 gap-3">
+            {categoryItems.map((categoryItem) => (
+              <CategoryCard data={categoryItem} key={categoryItem.id} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default demo;
