5e8ed6aa6696b793287c316ba760def67106b50b
Jahid0178 <zahidul.haque767@gmail.com>
2023-04-06 15:34:19 +0600

    added category card

--- pages/demo.js
+++ pages/demo.js
@@ -1,5 +1,20 @@
+import CategoryCard from "@/Components/CustomerComponents/Cards/CategoryCard/CategoryCard";
+import { categoryItems } from "@/data/data";
+
 const demo = () => {
-  return <div>demo</div>;
+  return (
+    <>
+      <section>
+        <div className="container mx-auto px-3">
+          <div className="grid grid-cols-6 gap-2">
+            {categoryItems.map((categoryItem) => (
+              <CategoryCard key={categoryItem.id} data={categoryItem} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default demo;
