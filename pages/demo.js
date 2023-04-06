fcfdded98b45b496642f97790ab16558d4b1a7bc
Jahid0178 <zahidul.haque767@gmail.com>
2023-04-06 23:36:30 +0600

    git config changed for added image domains

--- pages/demo.js
+++ pages/demo.js
@@ -1,12 +1,13 @@
 import CategoryCard from "@/Components/CustomerComponents/Cards/CategoryCard/CategoryCard";
 import { categoryItems } from "@/data/data";
+import Image from "next/image";
 
 const demo = () => {
   return (
     <>
       <section className="py-3">
         <div className="container mx-auto px-3">
-          <div className="grid grid-cols-6 gap-3">
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-3">
             {categoryItems.map((categoryItem) => (
               <CategoryCard data={categoryItem} key={categoryItem.id} />
             ))}
