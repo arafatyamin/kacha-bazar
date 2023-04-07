274155a8111b32c3a45197412a33573a2300e0ad
Sifytul <sifytul09@gmail.com>
2023-04-07 23:13:06 +0600

    adding padding

--- Components/CommonComponents/HeroSection.js
+++ Components/CommonComponents/HeroSection.js
@@ -21,22 +21,30 @@ const carouselSlider = [
 ]
 const HeroSection = () => {
   return (
-    <div>
-        
-      {/* left  */}
-      <div className="w-full md:w-2/3 ">
-        <Carousel
-        autoPlay={true}
-        infiniteLoop={true}
-        >
-          {carouselSlider.map((item) => (
-            <CarouselComponent heading={item.heading} subHeading={item.subheading} bgImage={item.imageURL}/>
-          ))}
-        </Carousel>
-      </div>
+    <div className="my-8  ">
+      <div className="max-w-screen-2xl mx-auto md:flex px-10 gap-8">
+        {/* left  */}
+        <div className="w-full md:w-3/5 rounded-md overflow-hidden">
+          <Carousel
+            autoPlay={true}
+            infiniteLoop={true}
+            showStatus={false}
+            emulateTouch={true}
+          >
+            {carouselSlider.map((item) => (
+              <CarouselComponent
+                heading={item.heading}
+                subHeading={item.subheading}
+                bgImage={item.imageURL}
+              />
+            ))}
+          </Carousel>
+        </div>
 
-      {/* right  */}
-      {/* <OfferComponent/> */}
+        {/* right  */}
+        <div className="w-2/5  bg-red-100"></div>
+        {/* <OfferComponent/> */}
+      </div>
     </div>
   );
 }
@@ -45,12 +53,12 @@ export default HeroSection
 
 const CarouselComponent = ({heading, subHeading,bgImage}) => {
     return (
-      <div className="relative text-left">
+      <div className="relative text-left rounded-md overflow-hidden">
         <img src={bgImage} />
-        <div className="absolute top-0 p-8 max-w-[490px]">
-          <h1 className='font-bold'>{heading}</h1>
-          <p className='text-gray-primary'>{subHeading}</p>
-          <Button text={"Shop Now"} fill={true} className="" />
+        <div className="absolute top-0  max-w-[490px] m-4 p-4 md:m-8 md:p-8 space-y-2">
+          <h1 className="font-bold text-2xl md:text-3xl">{heading}</h1>
+          <p className="text-gray-primary text-sm md:text-base">{subHeading}</p>
+          <Button text={"Shop Now"} fill={true} className="text-sm px-2 py-2" />
         </div>
       </div>
     );
