2046f05dd0f1ded111f76417ee8d62a799c1321a
Sifytul <sifytul09@gmail.com>
2023-04-09 01:43:59 +0600

    modified demo page

--- pages/demo.js
+++ pages/demo.js
@@ -1,19 +1,21 @@
 import CategoryCard from "@/Components/CustomerComponents/Cards/CategoryCard/CategoryCard";
+import ProductCard from "@/Components/CustomerComponents/Cards/ProductCard/ProductCard";
 import UserSideNav from "@/Components/CustomerComponents/CustomerAdmin/UserSideNav";
 import { categoryItems } from "@/data/data";
 
 const demo = () => {
   return (
     <>
-      <section className="py-3">
-        <div className="container mx-auto px-3">
-          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-3">
-            {categoryItems.map((categoryItem) => (
-              <CategoryCard data={categoryItem} key={categoryItem.id} />
-            ))}
-          </div>
-          <UserSideNav />
-        </div>
+      <section className="py-3 grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+        
+        <ProductCard imageURL={"/images/home/Aloe-Vera-Leaf-each.webp"} title={"Aloe Vera Leaf"} quantity={"each"} price={15} offer={8}/>
+        <ProductCard imageURL={"/images/home/Aloe-Vera-Leaf-each.webp"} title={"Aloe Vera Leaf"} quantity={"each"} price={15} offer={8}/>
+        <ProductCard imageURL={"/images/home/Aloe-Vera-Leaf-each.webp"} title={"Aloe Vera Leaf"} quantity={"each"} price={15} offer={8}/>
+        <ProductCard imageURL={"/images/home/Aloe-Vera-Leaf-each.webp"} title={"Aloe Vera Leaf"} quantity={"each"} price={15} offer={8}/>
+        <ProductCard imageURL={"/images/home/Aloe-Vera-Leaf-each.webp"} title={"Aloe Vera Leaf"} quantity={"each"} price={15} offer={8}/>
+        <ProductCard imageURL={"/images/home/Aloe-Vera-Leaf-each.webp"} title={"Aloe Vera Leaf"} quantity={"each"} price={15} offer={8}/>
+        <ProductCard imageURL={"/images/home/Aloe-Vera-Leaf-each.webp"} title={"Aloe Vera Leaf"} quantity={"each"} price={15} offer={8}/>
+        
       </section>
     </>
   );
