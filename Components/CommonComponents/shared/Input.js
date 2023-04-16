f3ad19e17eb810b40fc956e4479a2040c0f54202
Sifytul <sifytul09@gmail.com>
2023-04-17 04:52:47 +0600

    Added: Input Component

---
+++ Components/CommonComponents/shared/Input.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
+
+
+
+const Input = ({
+    className,
+  name,
+  placeholder,
+  Icon,
+  value,
+  changeHandler,
+  type,
+  ...props
+}) => {
+  const [passwordOpen, setPasswordOpen] = useState(false);
+  return (
+<div className={`flex items-center space-x-2 border border-gray p-2 rounded-lg ${className}`}>
+      {Icon && <Icon className="text-primary text-xl" />}
+      <input
+        className="outline-none flex-grow bg-inherit px-2 py-1 rounded-md"
+        name={name}
+        placeholder={placeholder}
+        type={type==="password" ? (passwordOpen ? "text" : "password") : type}
+        value={value}
+        onChange={changeHandler}
+        {...props}
+      />
+      {type==="password" && (
+        <div
+          className="text-primary text-xl cursor-pointer"
+          onClick={() => setPasswordOpen(!passwordOpen)}
+        >
+          {passwordOpen ? <BsEyeSlashFill /> : <BsEyeFill />}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Input;
