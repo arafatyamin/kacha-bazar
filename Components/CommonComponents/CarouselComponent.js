ba9964b89fc1cc9b6a6c39e003dfdcd5c7c3909b
Sifytul <sifytul09@gmail.com>
2023-04-18 18:01:00 +0600

    button will be hidden in mobile device

--- Components/CommonComponents/CarouselComponent.js
+++ Components/CommonComponents/CarouselComponent.js
@@ -5,13 +5,13 @@ const CarouselComponent = ({ heading, subHeading, bgImage }) => {
   return (
     <div className="relative text-left rounded-md overflow-hidden">
       <img src={bgImage} />
-      <div className="absolute top-0 w-2/3 m-3 p-3 md:m-6 md:p-6 lg:m-8 lg:p-8 space-y-2">
+      <div className="absolute top-0 w-2/3 m-2 p-2 md:m-6 md:p-6 lg:m-8 lg:p-8 space-y-2">
         <h1 className="font-bold text-xl sm:text-3xl">{heading}</h1>
         <p className="text-gray-primary text-xs md:text-base">{subHeading}</p>
         <Button
           text={"Shop Now"}
           fill={true}
-          className="text-xs md:text-base px-2 py-2 "
+          className="text-xs md:text-base px-2 py-2 hidden sm:inline"
         />
       </div>
     </div>
