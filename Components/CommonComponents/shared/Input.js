ab5a01812743972fa78d6ff86d2cc9bfcdd88d89
Sifytul <sifytul09@gmail.com>
2023-05-03 16:04:46 +0600

    modified Input design and added label

--- Components/CommonComponents/shared/Input.js
+++ Components/CommonComponents/shared/Input.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
 
 const Input = ({
+  label,
   className,
   name,
   placeholder,
@@ -13,27 +14,34 @@ const Input = ({
 }) => {
   const [passwordOpen, setPasswordOpen] = useState(false);
   return (
-    <div
-      className={`flex items-center space-x-2 border border-gray p-2 rounded-lg ${className}`}
-    >
-      {Icon && <Icon className="text-primary sm:text-xl" />}
-      <input
-        className="outline-none flex-grow bg-inherit px-2 py-1 rounded-md"
-        name={name}
-        placeholder={placeholder}
-        type={type === "password" ? (passwordOpen ? "text" : "password") : type}
-        value={value}
-        onChange={changeHandler}
-        {...props}
-      />
-      {type === "password" && (
-        <div
-          className="text-primary sm:text-xl cursor-pointer"
-          onClick={() => setPasswordOpen(!passwordOpen)}
-        >
-          {passwordOpen ? <BsEyeSlashFill /> : <BsEyeFill />}
-        </div>
-      )}
+    <div className="mb-4">
+      <label htmlFor={name} className="font-semibold text-gray-primary">{label}</label>
+      <div
+        className={`flex items-center gap-2 border border-gray  rounded-lg px-2 py-1 ${className}`}
+      >
+        {Icon && <Icon className="text-primary sm:text-xl hidden md:inline-block" />}
+        <input
+        autoComplete="false"
+          className="outline-none flex-1 px-2 h-8 rounded-md"
+          id={name}
+          name={name}
+          placeholder={placeholder}
+          type={
+            type === "password" ? (passwordOpen ? "text" : "password") : type
+          }
+          value={value}
+          onChange={changeHandler}
+          {...props}
+        />
+        {type === "password" && (
+          <div
+            className="text-primary sm:text-xl cursor-pointer"
+            onClick={() => setPasswordOpen(!passwordOpen)}
+          >
+            {passwordOpen ? <BsEyeSlashFill /> : <BsEyeFill />}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
