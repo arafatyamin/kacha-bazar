189733b88bd9af72c2874d867734ff8dab629a74
MD Ripon Islam <islam876ripon@gmail.com>
2023-05-04 10:51:54 +0600

    redux store added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import "@/styles/globals.css";
+import { Provider } from "react-redux";
+import { store } from "../store";
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 App;
