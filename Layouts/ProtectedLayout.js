e35cfcef0f6f33a6c9279d137599c486058e45fe
MD Ripon Islam <islam876ripon@gmail.com>
2023-05-05 18:46:26 +0600

    protected layout created

---
+++ Layouts/ProtectedLayout.js
@@ -0,0 +1,8 @@
+import React from "react";
+import withAuth from "../auth/withAuth";
+
+const ProtectedLayout = ({ children }) => {
+  return <>{children}</>;
+};
+
+export default withAuth(ProtectedLayout);
