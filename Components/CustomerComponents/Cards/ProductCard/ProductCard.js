d95a8507a94a9f1b16ad9bc53eccdd6dfa385a7f
Sifytul <sifytul09@gmail.com>
2023-05-07 13:02:59 +0600

    product card price and discount design modified

--- Components/CustomerComponents/Cards/ProductCard/ProductCard.js
+++ Components/CustomerComponents/Cards/ProductCard/ProductCard.js
@@ -1,15 +1,14 @@
 import Counter from "@/Components/CommonComponents/Counter";
 import Button from "@/Components/CommonComponents/shared/Button";
 import Image from "next/image";
-import {useState} from "react";
-import {MdShoppingBasket} from "react-icons/md";
+import { useState } from "react";
+import { MdShoppingBasket } from "react-icons/md";
 
 const ProductCard = ({data}) => {
-    const {title, images, price, quantity, discount} = data;
+    const {title, images, originalPrice, price, quantity, discount} = data;
 
     console.log(data);
 
-    const offerPrice = (price * (100 - discount)) / 100;
 
     {
         /**======== Counter part========= */
@@ -31,52 +30,52 @@ const ProductCard = ({data}) => {
         setHoverState(!hoverState);
     };
     return (
-        <div className="group bg-white min-w-[200px] relative rounded border border-gray-100 shadow-sm pt-4">
-            <div className="overflow-hidden">
-                <Image
-                    src={images[0]}
-                    alt={title}
-                    width={150}
-                    height={150}
-                    className="mx-auto group-hover:scale-110 duration-300"
-                />
-            </div>
-            {discount && (
-                <p className="absolute top-4 right-4 bg-orange-400 text-white text-sm w-fit px-2 py-[2px] rounded ">
-                    {discount}% Off
-                </p>
-            )}
-            <div className="px-4 pb-4">
-                <p className="text-gray-primary">{quantity || "N/R"}</p>
-                <p className="text-gray-primary">{title || "N/R"}</p>
-                <div className="flex justify-between items-center">
-                    {discount ? (
-                        <div className="flex gap-1">
-                            <p className="font-bold text-lg">${offerPrice}</p>
-                            <p className="font-bold line-through text-gray-primary">
-                                ${price}
-                            </p>
-                        </div>
-                    ) : (
-                        <p className="font-bold text-lg"> ${price}</p>
-                    )}
-                    {counter ? (
-                        <Counter
-                            value={counter}
-                            increaseHandler={increaseHandler}
-                            decreaseHandler={decreaseHandler}
-                        />
-                    ) : (
-                        <Button
-                            onClick={increaseHandler}
-                            Icon={MdShoppingBasket}
-                            className="p-1 border-[var(--clr-gray)] text-lg hover:bg-primary hover:text-white duration-200 hover:scale-105"
-                        />
-                    )}
-                </div>
-                <p>${price}</p>
-            </div>
+      <div className="group bg-white min-w-[200px] relative rounded border border-gray-100 shadow-sm pt-4">
+        <div className="overflow-hidden">
+          <Image
+            src={images[0]}
+            alt={title}
+            width={150}
+            height={150}
+            className="mx-auto group-hover:scale-110 duration-300"
+          />
         </div>
+        {discount &&  (
+          <p className="absolute top-4 right-4 bg-orange-400 text-white text-sm w-fit px-2 py-[2px] rounded font-medium ">
+            {isNaN(Number(discount)) ? <span className="text-lg">{discount} </span>: <span className="text-lg">${discount} </span>}
+            OFF
+          </p>
+        )}
+        <div className="px-4 pb-4">
+          <p className="text-gray-primary">{quantity || "N/R"}</p>
+          <p className="text-gray-primary">{title || "N/R"}</p>
+          <div className="flex justify-between items-center">
+            {discount ? (
+              <div className="flex gap-2">
+                <p className="font-bold text-lg">${price}</p>
+                <p className="font-semibold text-base line-through text-gray-primary">
+                  ${originalPrice}
+                </p>
+              </div>
+            ) : (
+              <p className="font-bold text-lg"> ${originalPrice}</p>
+            )}
+            {counter ? (
+              <Counter
+                value={counter}
+                increaseHandler={increaseHandler}
+                decreaseHandler={decreaseHandler}
+              />
+            ) : (
+              <Button
+                onClick={increaseHandler}
+                Icon={MdShoppingBasket}
+                className="p-1 border-[var(--clr-gray)] text-lg hover:bg-primary hover:text-white duration-200 hover:scale-105"
+              />
+            )}
+          </div>
+        </div>
+      </div>
     );
 };
 
