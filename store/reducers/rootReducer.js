ea5aa080a3a9e3dbe49dc56250cb107325d1a3f6
Sifytul <sifytul09@gmail.com>
2023-05-07 12:45:46 +0600

    removed unnecessary line

--- store/reducers/rootReducer.js
+++ store/reducers/rootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
 import { customerReducer } from "./customer";
-import productsReducers from "@/store/reducers/productsReducers/productsReducers";
 import cartReducer from "./cartReducer/cartReducer";
 
 const rootReducer = combineReducers({
   customer: customerReducer,
-  products: productsReducers,
   cart: cartReducer
 });
 
