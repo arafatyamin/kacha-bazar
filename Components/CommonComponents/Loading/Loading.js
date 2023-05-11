547ec3c0f9766acf28c387cc5036a24d6b85e052
Jahid0178 <zahidul.haque767@gmail.com>
2023-05-11 23:33:15 +0600

    added loading component when state change

---
+++ Components/CommonComponents/Loading/Loading.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { Zoom } from "react-preloaders2";
+
+const Loading = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const handleRouteChangeStart = () => setIsLoading(true);
+    const handleRouteComplete = () => setIsLoading(false);
+
+    router.events.on("routeChangeStart", handleRouteChangeStart);
+    router.events.on("routeChangeComplete", handleRouteComplete);
+    router.events.on("routeChangeError", handleRouteComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChangeStart);
+      router.events.off("routeChangeComplete", handleRouteComplete);
+      router.events.off("routeChangeError", handleRouteComplete);
+    };
+  }, []);
+  return (
+    <React.Fragment>{isLoading && <Zoom color={"#10b981"} />}</React.Fragment>
+  );
+};
+
+export default Loading;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import store from "@/store/store";
 import { Provider } from "react-redux";
 import "@/styles/globals.css";
+import Loading from "@/Components/CommonComponents/Loading/Loading";
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
 
   return getLayout(
     <Provider store={store}>
+      <Loading />
       <Component {...pageProps}> </Component>
     </Provider>
   );
