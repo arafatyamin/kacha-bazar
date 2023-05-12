06b24e34b49adb85c39d1e8e2738ef1b2c2e6a4f
MD Ripon Islam <islam876ripon@gmail.com>
2023-05-12 21:55:30 +0600

    custom redirect handler created

---
+++ auth/handleRedirect.js
@@ -0,0 +1,41 @@
+import isLoggedIn from "./isLoggedIn";
+
+const handleRedirect = async (context, role, props) => {
+  const { loggedIn, user } = await isLoggedIn(context);
+
+  if (!loggedIn) {
+    return {
+      redirect: {
+        destination: role === "admin" ? "/admin/login" : "/login",
+        permanent: false,
+      },
+    };
+  } else {
+    if (role === "admin") {
+      if (user.type === "customer") {
+        return {
+          redirect: {
+            destination: "/user",
+            permanent: false,
+          },
+        };
+      } else {
+        return { props: { ...props } };
+      }
+    }
+    if (role === "customer") {
+      if (user.type === "admin") {
+        return {
+          redirect: {
+            destination: "/admin/dashbord",
+            permanent: false,
+          },
+        };
+      } else {
+        return { props: { ...props } };
+      }
+    }
+  }
+};
+
+export default handleRedirect;
