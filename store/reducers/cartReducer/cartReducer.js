a667bc251def19f0bb94dbb0b9b54a5f5a0dd2dc
Jahid0178 <zahidul.haque767@gmail.com>
2023-05-14 18:41:01 +0600

    added add to cart and remove from cart in cart reducer

--- store/reducers/cartReducer/cartReducer.js
+++ store/reducers/cartReducer/cartReducer.js
@@ -9,41 +9,17 @@ const initialState = {
 };
 
 const cartReducer = (state = initialState, action) => {
+  console.log(action.payload);
   switch (action.type) {
     case ADD_TO_CART:
-      let isFound_in_add = state.cart.filter(
-        (item) => item.id == action.payload.id
-      );
-      if (isFound_in_add.length>0) {
-        isFound_in_add[0].count += 1;
-        return {
-          ...state,
-          cart: [...state.cart],
-        };
-      } else {
-        action.payload.count = 1;
-        return {
-          ...state,
-          cart: [...state.cart, action.payload],
-        };
-      }
-
-    case DECREASE_FROM_CART:
-      let isFound_in_decrease = state.cart.filter(
-        (item) => item.id == action.payload.id
-      );
-      isFound_in_decrease[0].count -= 1;
       return {
         ...state,
-        cart: [...state.cart],
+        cart: [...state.cart, action.payload],
       };
     case REMOVE_FROM_CART:
-      let newState = state.cart.filter(
-        (item) => item.id !== action.payload.id
-      );
       return {
         ...state,
-        cart: [...newState],
+        cart: state.cart.filter((product) => product.id !== action.payload.id),
       };
     default:
       return state;
