ba600897fa72f5b5c864b86ca70b8c7a04a05edb
MD Ripon Islam <islam876ripon@gmail.com>
2023-05-17 19:58:16 +0600

    coupons reducer added , imports updated

--- store/reducers/admin/index.js
+++ store/reducers/admin/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
-import productReducer from "./productsReducer.js";
-import categoryReducer from "./categoriesReducer.js";
+import productReducer from "./productsReducer";
+import categoryReducer from "./categoriesReducer";
+import couponReducer from "./couponsReducer";
 
 const adminReducer = combineReducers({
   products: productReducer,
   categories: categoryReducer,
+  coupons: couponReducer,
 });
 
 export default adminReducer;
