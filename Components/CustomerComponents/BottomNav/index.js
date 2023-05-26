36b701cb20f6f8f38bae887ba901e60545fc71e8
Sifytul <sifytul09@gmail.com>
2023-05-26 17:18:30 +0600

    added: cart length

--- Components/CustomerComponents/BottomNav/index.js
+++ Components/CustomerComponents/BottomNav/index.js
@@ -6,12 +6,13 @@ import { BsFillPersonFill } from 'react-icons/bs';
 import { CgMenuMotion } from "react-icons/cg";
 import { FaHome } from 'react-icons/fa';
 import { RiShoppingCartFill } from 'react-icons/ri';
+import { useSelector } from 'react-redux';
 
 const BottomNav = () => {
   const loggedIn =true
     const [openDrawer, setOpenDrawer] = useState(false);
     const drawerHandler = () => setOpenDrawer(!openDrawer)
-
+    const cartLength = useSelector(state=>state.cart.cart.length)
   return (
     <div className="bg-primary lg:hidden fixed bottom-0 w-full z-10">
       <div className="container">
@@ -29,7 +30,10 @@ const BottomNav = () => {
           <Link href={"/"}>
             <FaHome className="hover:scale-105 duration-200 cursor-pointer" />
           </Link>
-          <Link href={"/checkout"}>
+          <Link href={"/checkout"} className="relative  w-8">
+            <span className="absolute -top-2 -right-1 bg-red-500 w-5 h-5 text-center rounded-full text-sm z-10">
+              {cartLength}
+            </span>
             <RiShoppingCartFill className="hover:scale-105 duration-200 cursor-pointer" />
           </Link>
           {!loggedIn ? (
