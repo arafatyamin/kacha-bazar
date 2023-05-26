45a0f29566fe0fa1a52781ae5a531c78a90ebac0
Sifytul <sifytul09@gmail.com>
2023-05-26 14:20:55 +0600

    bug: fixed ref related issue through forwardRef

--- Components/CommonComponents/shared/Input.js
+++ Components/CommonComponents/shared/Input.js
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { forwardRef, useState } from "react";
 import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
 
-const Input = ({ label, id, className, placeholder, Icon, type, ...props }) => {
+const Input = forwardRef(({ label, id, className, placeholder, Icon, type, ...props },ref) => {
   const [passwordOpen, setPasswordOpen] = useState(false);
   return (
     <div>
@@ -11,24 +11,25 @@ const Input = ({ label, id, className, placeholder, Icon, type, ...props }) => {
         </label>
       )}
       <div
-        className={`flex items-center gap-2 border border-gray  rounded-lg px-2 py-1 ${className}`}
+        className={`flex items-center gap-1 md:gap-2 border border-gray  rounded-lg px-2 py-1 ${className}`}
       >
         {Icon && (
           <Icon className="text-primary sm:text-xl hidden md:inline-block" />
         )}
         <input
           autoComplete="false"
-          className="outline-none flex-1 px-2 h-8 rounded-md"
+          className="outline-none flex-1 px-2 h-8 rounded-md min-w-[80%]"
           id={id}
           placeholder={placeholder}
           type={
             type === "password" ? (passwordOpen ? "text" : "password") : type
           }
+          ref={ref}
           {...props}
         />
         {type === "password" && (
           <div
-            className="text-primary sm:text-xl cursor-pointer"
+            className="text-primary sm:text-xl cursor-pointer flex-shrink-0"
             onClick={() => setPasswordOpen(!passwordOpen)}
           >
             {passwordOpen ? <BsEyeSlashFill /> : <BsEyeFill />}
@@ -37,6 +38,6 @@ const Input = ({ label, id, className, placeholder, Icon, type, ...props }) => {
       </div>
     </div>
   );
-};
+})
 
 export default Input;
