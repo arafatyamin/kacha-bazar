5d649c0d8a163a088cb1d769fb1f3d6395f0acff
Sifytul <sifytul09@gmail.com>
2023-05-26 14:23:39 +0600

    Revert "fix: fixed ref related issue through forwardRef"
    This reverts commit 45a0f29566fe0fa1a52781ae5a531c78a90ebac0.

--- Components/CommonComponents/shared/Input.js
+++ Components/CommonComponents/shared/Input.js
@@ -1,7 +1,7 @@
-import { forwardRef, useState } from "react";
+import { useState } from "react";
 import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
 
-const Input = forwardRef(({ label, id, className, placeholder, Icon, type, ...props },ref) => {
+const Input = ({ label, id, className, placeholder, Icon, type, ...props }) => {
   const [passwordOpen, setPasswordOpen] = useState(false);
   return (
     <div>
@@ -11,25 +11,24 @@ const Input = forwardRef(({ label, id, className, placeholder, Icon, type, ...pr
         </label>
       )}
       <div
-        className={`flex items-center gap-1 md:gap-2 border border-gray  rounded-lg px-2 py-1 ${className}`}
+        className={`flex items-center gap-2 border border-gray  rounded-lg px-2 py-1 ${className}`}
       >
         {Icon && (
           <Icon className="text-primary sm:text-xl hidden md:inline-block" />
         )}
         <input
           autoComplete="false"
-          className="outline-none flex-1 px-2 h-8 rounded-md min-w-[80%]"
+          className="outline-none flex-1 px-2 h-8 rounded-md"
           id={id}
           placeholder={placeholder}
           type={
             type === "password" ? (passwordOpen ? "text" : "password") : type
           }
-          ref={ref}
           {...props}
         />
         {type === "password" && (
           <div
-            className="text-primary sm:text-xl cursor-pointer flex-shrink-0"
+            className="text-primary sm:text-xl cursor-pointer"
             onClick={() => setPasswordOpen(!passwordOpen)}
           >
             {passwordOpen ? <BsEyeSlashFill /> : <BsEyeFill />}
@@ -38,6 +37,6 @@ const Input = forwardRef(({ label, id, className, placeholder, Icon, type, ...pr
       </div>
     </div>
   );
-})
+};
 
 export default Input;
