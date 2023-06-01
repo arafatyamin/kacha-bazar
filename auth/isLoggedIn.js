717f548bc1dacd621bcda5f59ca97784b1c6a54e
Ripon876 <islam876ripon@gmail.com>
2023-06-02 01:47:41 +0600

    returning token with user info

--- auth/isLoggedIn.js
+++ auth/isLoggedIn.js
@@ -2,9 +2,12 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 const isLoggedIn = async (ctx) => {
-  const session = await getServerSession(ctx.req, ctx.res, authOptions);
+  const { req, res } = ctx;
+  const session = await getServerSession(req, res, authOptions);
+  const token = req.cookies["next-auth.session-token"];
+
   return session
-    ? { loggedIn: true, user: { ...session.user } }
+    ? { loggedIn: true, user: { ...session.user }, token }
     : { loggedIn: false };
 };
 
